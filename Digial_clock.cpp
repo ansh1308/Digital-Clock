e2485428b9b3f9433b528c5749f5c31165198f59
ansh1308 <76175981+ansh1308@users.noreply.github.com>
2022-02-03 22:37:06 +0530

    Add files via upload

---
+++ Digial_clock.cpp
@@ -0,0 +1,67 @@
+#include <iomanip>
+#include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+using namespace std;
+  
+int main()
+{
+    system("color 4A");
+  
+    int hour, min, sec;
+    cout << setw(70)
+         << "*Enter Current time*\n";
+  
+    cout << "HH- ";
+    cin >> hour;
+    cout << "MM- ";
+    cin >> min;
+    cout << "SS- ";
+    cin >> sec;
+  
+    system("color 4A");
+  
+    if (hour > 23) {
+        cout << "Wrong Time input";
+    }
+    else if (min > 60) {
+        cout << "Wrong Time Input";
+    }
+    else if (sec > 60) {
+        cout << "Wrong Time Input";
+    }
+  
+    else {
+        while (1)
+  
+        {
+            system("cls");
+  
+            for (hour; hour < 24; hour++) {
+  
+                for (min; min < 60; min++) {
+  
+                    for (sec; sec < 60; sec++) {
+                        system("cls");
+  
+                        cout << "\n\n\n\n~~~~~~~~~"
+                                "~~~~~~~~~~~~~~~~~~~~~"
+                                "~~~~~~~~~~~~~~~~~~"
+                                "Current Time = "
+                             << hour << ":" << min << ":"
+                             << sec
+                             << "Hrs~~~~~~~~~~~~~~~~~~"
+                                "~~~~~~~~~~~~~~~~~~~~~"
+                                "~~~~~~~~~";
+  
+  
+                        Sleep(1000);
+  
+                    }
+                    sec = 0;
+                }
+                min = 0;
+            }
+        }
+    }
+}
